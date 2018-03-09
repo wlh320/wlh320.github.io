735bd572eb4acdd8be4d5a8d455704ff21115f6c
w0h <54wlh@tongji.edu.cn>
2018-03-09 15:52:07 +0800

    remove unneceassary jquery dependency

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // js
-import $ from 'jquery'
+//import $ from 'jquery'
 import CodeMirror from 'codemirror/lib/codemirror'
 import marked from 'marked'
 import malarkey from 'malarkey'
@@ -16,7 +16,7 @@ import '../css/style.css'
 
 // content
 var content = `
-![avatar](https://tvax2.sinaimg.cn/crop.0.0.664.664.180/005NVR7Rly8fkiq65xr5ij30ig0igjrq.jpg)
+![avatar](https://tvax2.sinaimg.cn/crop.0.0.600.600.180/005NVR7Rly8fnyzlk0r1kj30go0goab3.jpg)
 
 欢迎来到 zilcH40 的个人主页
 ===========================
@@ -27,33 +27,31 @@ Hi! 这里是 **zilcH40** 的个人主页
 
 目前是 CS Undergraduate && Newbie Developer
 
-- Arch Linux user
-- Vim fan
-- {C\\ , C++\\ , Python\\ }Programmer
+- {C\\ ,C++\\ ,Python\\ }Programmer
 - 萌新 Gopher
 - 会一丢丢前端 (比如攒一个像这样的页面 \\_(:з」∠)\\_ )
+- Arch Linux user
+- Vim fan
 
 ## Contact Me
 
 你可以从下面这些地方找到我
 
 - [GitHub](https://www.github.com/wlh320)
-- [博客园](https://www.cnblogs.com/VV0H)
-- [Steam](http://steamcommunity.com/id/wlh233)
+- [个人博客](https://blog.zilch40.wang)
+- [Steam](https://steamcommunity.com/id/wlh233)
 - [新浪微博](https://weibo.com/VV0H)
-- [网易云音乐](http://music.163.com/#/playlist?id=52197676)
+- [网易云音乐](https://music.163.com/#/playlist?id=52197676)
 - [给我发email](mailto:zilch40.wang@gmail.com)
 
-ok, 有了上面的链接, 你就可以轻而易举地人肉我了 XD
+Copyright ©2018 zilcH40
 
-### Please give me some suggestions!
-
-Copyright ©2017 zilcH40
+[沪ICP不备 2333333号-2](http://www.miitbeian.gov.cn)
 
 `
 
 // CodeMirror editor
-var editor = CodeMirror.fromTextArea($('#editor').get(0), {
+var editor = CodeMirror.fromTextArea(document.querySelector('#editor'), {
   mode: 'markdown',
   lineNumbers: true,
   lineWrapping: true,
@@ -80,8 +78,7 @@ marked.setOptions({
 });
 
 function editorOnChange(cm, co) {
-  $('.markdown-body').html(marked(cm.getValue()));
-  $('.preview-wrap').scrollTop($('.preview-wrap')[0].scrollHeight);
+  document.querySelector('.markdown-body').innerHTML = marked(cm.getValue());
   cm.scrollTo(0, cm.getScrollInfo().height);
 }
 
@@ -100,3 +97,4 @@ var type_opts = {
 };
 
 malarkey(editor, type_opts).type(content);
+
